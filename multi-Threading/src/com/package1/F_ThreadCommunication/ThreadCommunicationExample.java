79ab0c527901fe3444c6c22602f46600148e6bdb
chavansanket <sanketc1297@gmail.com>
2025-02-03 09:18:43 +0530

    Thread communication update

--- multi-Threading/src/com/package1/F_ThreadCommunication/ThreadCommunicationExample.java
+++ multi-Threading/src/com/package1/F_ThreadCommunication/ThreadCommunicationExample.java
@@ -1,7 +1,30 @@
 package com.package1.F_ThreadCommunication;
 
 
-
+class ShraredResource{
+	
+	private int data;
+	private boolean hasData;
+	
+	public void produce(int value) {
+		while(hasData) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+		data = value;
+		hasData = true;
+        System.out.println("Produced: " + value);
+        notify();
+	}
+	
+	
+	public int consume() {
+		return data;
+	}
+}
 
 public class ThreadCommunicationExample {
 
